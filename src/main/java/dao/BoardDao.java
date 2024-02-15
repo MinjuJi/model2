ff65f158cd20d6997c77c3de23f130f98c922061
MinjuJi <hinote444@gmail.com>
2024-02-15 19:28:11 +0900

    feat: 게시판 폼 개발

---
+++ src/main/java/dao/BoardDao.java
@@ -0,0 +1,32 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+import ibatis.IbatisUtil;
+import vo.Board;
+
+public class BoardDao {
+	
+	private final SqlMapClient ibatis = IbatisUtil.getSqlMapClient();
+	
+	@SuppressWarnings("unchecked")
+	public List<Board> getAllBoards() throws SQLException {
+		return (List<Board>) ibatis.queryForList("boards.getAllBoards");
+	}
+	
+	public List<Board> getBoardByNo(int no) throws SQLException {
+		return (List<Board>) ibatis.queryForObject("boards.getBoardByNo", no);
+	}
+	
+	public void insertBoard(Board board) throws SQLException {
+		ibatis.insert("boards.insertBoard", board);
+	}
+	
+	public void updateBoard(Board board) throws SQLException {
+		ibatis.update("boards.updateBoard", board);
+	}
+	
+}
